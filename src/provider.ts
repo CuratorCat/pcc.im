138e4ae6b35ac6ac39b2411a9b63e7d3db0ffff5
curatorcat <102763039+curatorcat@users.noreply.github.com>
2022-06-02 05:26:50 -0700

    update

--- src/provider.ts
+++ src/provider.ts
@@ -1,5 +1,5 @@
 import { ethers } from 'ethers'
 
 export const provider = process.env.useWebSocket
-  ? ethers.providers.InfuraProvider.getWebSocketProvider()
+  ? ethers.providers.InfuraProvider.getWebSocketProvider(1, process.env.infuraKey)
   : new ethers.providers.InfuraProvider(1, process.env.infuraKey)
