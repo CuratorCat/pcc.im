d66198ff2dc8de342a8bc242cda9cae0421bd9d4
curatorcat <102763039+curatorcat@users.noreply.github.com>
2022-08-23 05:50:53 -0700

    refactor(avatar): add http avatar url check

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,13 +1,13 @@
-
 import { CloudIcon } from '@heroicons/react/solid'
 import { useState } from 'react'
+import { isHttpProtocol, formatUrl } from 'functions/SocialHelpers'
 export function Avatar(props) {
   if (props.avatar == null) {
     return (
       <div className="relative w-20 h-20 sm:w-24 sm:h-24 rounded-full overflow-hidden">
         <div className="absolute inset-0">
           <div className="flex h-full bg-black/20 rounded-full overflow-hidden backdrop-blur-sm">
-              <div className="h-4 w-4 m-auto bg-violet-400 rounded-full animate-ping" />
+            <div className="h-4 w-4 m-auto bg-violet-400 rounded-full animate-ping" />
           </div>
         </div>
       </div>
@@ -23,32 +23,33 @@ export function Avatar(props) {
       </div>
     )
   } else {
-    return (
-      <>
-        {ImgLoader(props.ens, props.catId)}
-      </>
-    )
+    return <>{ImgLoader(props.ens, props.catId, props.avatar)}</>
   }
 }
 
-
-function ImgLoader(ens, catId) {
+function ImgLoader(ens, catId, avatar) {
   if (!ens) return null
   const [loaded, setLoaded] = useState(false)
-  // special for PCC Cats, loads faster
-  const avatarUrl = catId ? 'https://raw.githubusercontent.com/CuratorCat/pcc-cats-jpg/main/w1000/' + catId + '.jpg' : 'https://metadata.ens.domains/mainnet/avatar/' + ens
+
+  const avatarUrl = catId
+    ? // use special url for PCC Cats for faster loading ux
+      'https://raw.githubusercontent.com/CuratorCat/pcc-cats-jpg/main/w1000/' + catId + '.jpg'
+    : isHttpProtocol(avatar)
+    ? // use direct https for http protocol
+      formatUrl(avatar)
+    : // use metadata.ens.domains for others
+      'https://metadata.ens.domains/mainnet/avatar/' + ens
 
   return (
     <>
-      <div className='w-20 h-20 sm:w-24 sm:h-24 rounded-full'>
-      <img
-        src={avatarUrl}
-        className="w-full h-full rounded-full overflow-hidden"
-        onLoad={() => setLoaded(true)}
-        alt=""
-      />
+      <div className="w-20 h-20 sm:w-24 sm:h-24 rounded-full">
+        <img
+          src={avatarUrl}
+          className="w-full h-full rounded-full overflow-hidden"
+          onLoad={() => setLoaded(true)}
+          alt=""
+        />
       </div>
-      
       {loaded ? null : (
         // loading image
         <div className="absolute inset-0">
@@ -59,7 +60,6 @@ function ImgLoader(ens, catId) {
           </div>
         </div>
       )}
-      
     </>
   )
 }
